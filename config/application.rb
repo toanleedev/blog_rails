7e0ec4bffc6c4d07a0aeb7cd15c4948c0b22774f
rikai-toanle <toan.le@rikai.technology>
2024-05-06 14:57:41 +0700

    Scaffold post

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,11 @@ module BlogApp
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+    config.generators do |g|
+      g.assets            false
+      g.helper            false
+      g.test_framework    nil
+      g.jbuilder          false
+    end
   end
 end
